10a0c93fc146b9c4a228fa117ccbb69784893c77
Ahmed <andamanhoury@gmail.com>
2024-05-31 19:31:28 -0700

    a humble start

---
+++ e-buy-front/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      This is a start
+    </>
+  )
+}
+
+export default App
