2a879edc0bb81316b37877006663fdca1afea4af
Abdullahi Mohamed <amohamed2096@sdsu.edu>
2024-05-31 21:31:51 -0500

    spring boot setup

---
+++ e-buy-backend/src/main/java/com/shop/e_buy_backend/EBuyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.shop.e_buy_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBuyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EBuyBackendApplication.class, args);
+	}
+
+}
