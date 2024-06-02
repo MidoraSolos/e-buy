4a937f112fa1916bf80d7667be26f550b17101b9
Ahmed <andamanhoury@gmail.com>
2024-06-01 18:17:48 -0700

    added sign up and sign in page

---
+++ e-buy-front/src/Components/SignIn.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import "../CSS/SignIn.css";
+
+import { Link } from "react-router-dom";
+
+const SignIn = () => {
+	return (
+		<div className="SignUp template d-flex justify-content-center align-items-center vh-100 bg-primary">
+			<div className="form_containter p-5 rounded bg-white">
+				<form>
+					<h3 className="text-center">Sign In</h3>
+					<div className="mb-2">
+						<label htmlFor="email">Email</label>
+						<input
+							type="email"
+							placeholder="Enter Email"
+							className="form-control"
+						/>
+					</div>
+					<div className="mb-2">
+						<label htmlFor="password">Password</label>
+						<input
+							type="password"
+							placeholder="Enter Password"
+							className="form-control"
+						/>
+					</div>
+					<div className="mb-2">
+						<input
+							type="checkbox"
+							className="custom-control custom-checkbox"
+							id="check"
+						/>
+						<label htmlFor="check" className="custom-input-label ms-2">
+							Remember me
+						</label>
+					</div>
+					<div className="d-grid ">
+						<button className="btn btn-primary">Sign In</button>
+					</div>
+				</form>
+				<p className="text-end mt-2">
+					Forget{" "}
+					<a href="" className="">
+						Password?
+					</a>{" "}
+					<Link to="/SignUp" className="ms-2">
+						Sign Up
+					</Link>
+				</p>
+			</div>
+		</div>
+	);
+};
+export default SignIn;
---
+++ e-buy-front/src/Routes/BaseRouter.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import SignIn from "../Components/SignIn";
+import SignUp from "../Components/SignUp";
+
+const BaseRouter = () => {
+	return (
+		<Router>
+			<Routes>
+				<Route path="/SignIn" element={<SignIn />}></Route>
+				<Route path="/SignUp" element={<SignUp />}></Route>
+			</Routes>
+		</Router>
+	);
+};
+
+export default BaseRouter;
